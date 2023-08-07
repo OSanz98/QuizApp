525a6468b896e60ff1f272401ccbcfe6b121ef7c
OSanz98 <oscar.sanz@prographicconsultants.com>
2023-08-07 19:36:00 +0100

    Update Network API
    Updated implementation

--- app/src/main/java/sanzlimited/com/quizapp/data/implementation/QuestionRepositoryImpl.kt
+++ app/src/main/java/sanzlimited/com/quizapp/data/implementation/QuestionRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package sanzlimited.com.quizapp.data.implementation
 
+import android.util.Log
 import sanzlimited.com.quizapp.data.DataOrException
 import sanzlimited.com.quizapp.data.model.QuestionItem
 import sanzlimited.com.quizapp.data.network.QuestionApi
@@ -10,13 +11,34 @@ import javax.inject.Inject
 class QuestionRepositoryImpl @Inject constructor(
     private val api: QuestionApi
 ): QuestionRepository {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
 
-    override suspend fun getAllDifficultyQuestions(category: String) {
-        TODO("Not yet implemented")
+    override suspend fun getAllDifficultyQuestions(category: String): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            //setting loading to true so we can show a loading screen to let user know that a request is happening
+            dataOrException.loading = true
+            dataOrException.data = api.getAllDifficultyQuestions(category)
+            //when we get data then set loading to false to show we got a response
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllDifficultyQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
     }
 
-    override suspend fun getSpecificDifficultyQuestions(category: String, difficulty: String) {
-        TODO("Not yet implemented")
+    override suspend fun getSpecificDifficultyQuestions(category: String, difficulty: String): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            //setting loading to true so we can show a loading screen to let user know that a request is happening
+            dataOrException.loading = true
+            dataOrException.data = api.getSpecificDifficultyQuestions(category, difficulty)
+            //when we get data then set loading to false to show we got a response
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllDifficultyQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
     }
+
 }
--- app/src/main/java/sanzlimited/com/quizapp/data/repository/QuestionRepository.kt
+++ app/src/main/java/sanzlimited/com/quizapp/data/repository/QuestionRepository.kt
@@ -1,6 +1,10 @@
 package sanzlimited.com.quizapp.data.repository
 
+import sanzlimited.com.quizapp.data.DataOrException
+import sanzlimited.com.quizapp.data.model.QuestionItem
+import java.lang.Exception
+
 interface QuestionRepository {
-    suspend fun getAllDifficultyQuestions(category: String)
-    suspend fun getSpecificDifficultyQuestions(category: String, difficulty: String)
+    suspend fun getAllDifficultyQuestions(category: String): DataOrException<ArrayList<QuestionItem>, Boolean, Exception>
+    suspend fun getSpecificDifficultyQuestions(category: String, difficulty: String): DataOrException<ArrayList<QuestionItem>, Boolean, Exception>
 }
